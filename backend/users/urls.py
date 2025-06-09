b18b1c38fc42458062d576a7ba97afd92f7925b5
Noah Amoo <yawnoah3n@yahoo.com>
2025-06-09 16:53:04 +0100

    Added a path to register new user

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserInfoview
+from .views import UserInfoview, UserRegistrationView
 
 urlpatterns = [
-    path("user-info/", UserInfoview.as_view(), name="user-info")
+    path("user-info/", UserInfoview.as_view(), name="user-info"),
+    path("register/", UserRegistrationView.as_view(), name="register-user" )
 ]
