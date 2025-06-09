ed6cfdfeb20cc11a975478b9f5ffd29104d47a3b
Noah Amoo <yawnoah3n@yahoo.com>
2025-06-09 16:35:40 +0100

    Created a register serializer in serializers to create new users

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,3 +5,13 @@ class CustomUserSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ("id", "email", "username")
+
+class RegisterUserSerializer(ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ("email", "username", "password")
+        extra_kwargs = {"password": {"write_only":True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
